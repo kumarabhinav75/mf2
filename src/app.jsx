34fa4ac93977f5c63301a84c6b1230c2517c9314
abhinavkumar75 <abhinav.kumar@cleartax.in>
2024-01-22 03:16:41 +0530

    initial setup mf2

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Style from './style.css';
+
+const App = () => {
+
+  return (
+    <div className={'mf2'}>
+      <h1>MF2 Application</h1>
+    </div>
+  );
+};
+
+export default App;
